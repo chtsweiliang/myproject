0ac70a939509d9cc1d83795f0a8c1f088b4c05f0
William Chen <chtsweiliang@gmail.com>
2021-07-17 20:34:24 +0800

    OO version of scoring

--- src/main/java/com/cht/Scoring.java
+++ src/main/java/com/cht/Scoring.java
@@ -1,49 +1,34 @@
 package com.cht;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scoring {
-    public static void main(String[] args) {
-
-        String[] names = new String[4];
-        int[] maths = new int[4];
-        int[] englishs = new int[4];
-        try {
-            FileReader fileReader = new FileReader("data.txt");
-            BufferedReader br = new BufferedReader(fileReader);
-            String line = br.readLine();
-            int i = 0;
-            while (line != null) {
-//                Eric 90 70
+        public static void main(String[] args) {
+            List<Student> students = new ArrayList<>();
+            try {
+                FileReader fileReader = new FileReader("data.txt");
+                BufferedReader br = new BufferedReader(fileReader);
+                String line = br.readLine();
+                int i = 0;
+                while(line != null) {
+                    //Eric 90 70
 //                System.out.println(line);
-                String[] tokens = line.split(" ");
-                names[i] = tokens[0];
-                maths[i] = Integer.parseInt(tokens[1]);
-                englishs[i] = Integer.parseInt(tokens[2]);
-                int average = (englishs[i] + maths[i]) / 2;
-                String grading = "F";
-
-                if (average >= 90) {
-                    grading = "A";
-                } else if (average >= 80) {
-                    grading = "B";
-                } else if (average >= 70) {
-                    grading = "C";
-                } else if (average >= 60) {
-                    grading = "D";
+                    String[] tokens = line.split(" ");
+                    Student stu = new Student(tokens[0],
+                            Integer.parseInt(tokens[1]),
+                            Integer.parseInt(tokens[2]));
+                    System.out.println(stu.name + " " + stu.english + " " + stu.math + " "
+                            + stu.getAverage() + " " + stu.getGrading());
+                    students.add(stu);
+                    line = br.readLine();
+                    i++;
                 }
-
-
-                System.out.println(names[i] + " " + englishs[i] + " " + maths[i] + " " + average + " " + grading);
-
-                line = br.readLine();
-                i++;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-    }
 }
